b607e79c8579a0a556fd9a71502b8190b4e9bfe9
JesusCadenasA <146914245+JCA9@users.noreply.github.com>
2024-05-14 17:38:03 -0600

    BreastCancer con 10-Fold Cross-Validation estratificado.py

---
+++ Lab9/BreastCancer-10Fold.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import *
+from sklearn.model_selection import StratifiedKFold
+import numpy as np
+
+# Conjunto de datos Breast Cancer Wisconsin
+breast_cancer = load_breast_cancer()
+X = breast_cancer.data
+y = breast_cancer.target
+
+# Objeto StratifiedKFold con 10 folds
+skf = StratifiedKFold(n_splits=10)
+
+# Se itera sobre los folds y se realiza la validacion cruzada
+for train_index, test_index in skf.split(X, y):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+
+    # Verificar la proporcion de clases en los conjuntos de entrenamiento y prueba para cada fold
+    print("Fold:")
+    print("Proporcion de clases en el conjunto de entrenamiento:")
+    print(np.bincount(y_train))
+    print("Proporcion de clases en el conjunto de prueba:")
+    print(np.bincount(y_test))
